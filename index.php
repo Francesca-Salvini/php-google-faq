7dbb4620987b93d61db1b18c15c152a8c532f210
Francesca Salvini <franci98sp@gmail.com>
2021-05-19 16:21:36 +0200

    scaffolding + header

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Faq</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- GOOGLE FONTS : ROBOTO -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+    <header>
+        <div class="logo">
+            <img src="img/google.png" alt="">
+
+            <h2>Privacy e termini</h2>
+        </div>
+
+        
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Introduzione</a>
+                </li>
+                <li>
+                    <a href="#">Norme sulla privacy</a>
+                </li>
+                <li>
+                    <a href="#">Termini di servizio</a>
+                </li>
+                <li>
+                    <a href="#">Tecnologie</a>
+                </li>
+                <li>
+                    <a id="blue" href="#">Domande frequenti</a>
+                </li>
+            </ul>
+        </nav>
+
+        <hr>
+    </header>
+    
+</body>
+</html>
